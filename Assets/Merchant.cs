256eba5db91f8d885b7ddc834c492864aac0a9a5
Tristan Maindron <tmaindron@gmail.com>
2019-04-28 17:25:13 +0200

    Initial work on the merchant

---
+++ Assets/Merchant.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Merchant : MonoBehaviour {
+
+    [TextAreaAttribute(3, 10)]
+    public string[] messages;
+    Queue<string> queue;
+
+    public GameObject text;
+    string currentMessage = null;
+
+    // Use this for initialization
+    void Start () {
+        queue = new Queue<string>(messages);
+        ShowMessage();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ShowMessage()
+    {
+        if (queue.Count > 0)
+        {
+            currentMessage = queue.Dequeue();
+            StartCoroutine(TypeSentence());
+        }
+    }
+
+    IEnumerator TypeSentence()
+    {
+        text.GetComponent<Text>().text = "";
+        int i = 0;
+
+        foreach (char letter in currentMessage.ToCharArray())
+        {
+            text.GetComponent<Text>().text += letter;
+            if (i % 3 == 0)
+            {
+                yield return null;
+            }
+
+            i++;
+        }
+    }
+}
