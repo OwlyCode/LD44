9ef3f262ac29e2d36125793f9301f1130f71746f
Tristan Maindron <tmaindron@gmail.com>
2019-04-29 03:08:17 +0200

    Added sound when exploding or using shield.

--- Assets/Ship.cs
+++ Assets/Ship.cs
@@ -7,18 +7,32 @@ public class Ship : MonoBehaviour {
 
     public GameObject brokenVersion;
 
+    public AudioClip explosion;
+    public AudioClip shield;
+
     private void OnCollisionEnter(Collision collision)
     {
         if (GlobalState.hasEmergencyShieldUpgrade && (GlobalState.emergencyShieldLife > 0))
         {
             GlobalState.emergencyShieldLife--;
             collision.gameObject.GetComponent<Rigidbody>().AddExplosionForce(1000f, transform.position, 5000f);
+            GetComponentInParent<AudioSource>().clip = shield;
+            GetComponentInParent<AudioSource>().Play();
+
 
             return;
         }
 
         GameObject destroyed = Instantiate(brokenVersion, transform.position, transform.rotation);
         destroyed.transform.parent = transform.parent;
+
+        AudioSource s = GetComponentInParent<AudioSource>();
+
+        if (s != null)
+        {
+            s.clip = explosion;
+            s.Play();
+        }
         Destroy(gameObject);
 
         GameObject.Find("LevelController").GetComponent<LevelController>().alive = false;
